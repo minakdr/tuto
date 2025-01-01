7ee674d3d91d9f0ad822d8d7d3a7d3c3f02d58b9
minakdr <mina125.ak@gmail.com>
2025-01-01 19:21:53 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './App.css';
 
 
+const Nav = () => {
+  return ( 
+    <h1> this is a nav</h1>
+  )
+  }
+
+
+const MainContent = () => {
+  return (
+    <h1> this is content </h1>
+  )
+}
+
+
 const MyInfos = () => {
 return (
   <div>
@@ -15,20 +29,32 @@ return (
           </ul>
   </div>
 )
-
 }
+
+
+const Footer = () => {
+  return ( 
+    <h1> this is a footer</h1>
+  )
+  }
+
 
 function App() {
   return ( 
     <div> {/* had to use  div because h1 and p are sibling elements and the retun needs to only return one parent element so I had to wrap them into 1 div  */}
+          <Nav/>
           <h1>Hello World</h1> 
           <p>this a paragrph</p> 
+
           <ul style={{ listStyleType: "none" }}> {/* Inline CSS also needs to be in camelCase */}
             <li>first list item</li>
             <li>second list item</li>
             <li>third list item</li>
           </ul>
           <MyInfos/>
+          <MainContent/>
+          <Footer/>
+
     </div>
 
   );
