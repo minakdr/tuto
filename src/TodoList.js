a98f1c6d842103925040ccb1ee31ac1965c84830
minakdr <mina125.ak@gmail.com>
2025-01-01 20:25:47 +0100

    d

---
+++ src/TodoList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './App.css';
+
+
+
+
+
+function TodoList() {
+return ( 
+<h1>hey</h1>
+
+);
+}
+
+export default TodoList;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+import TodoList from './TodoList';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<App />);
+root.render(<TodoList />);
 
 
