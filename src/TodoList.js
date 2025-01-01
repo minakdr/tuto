86f09da198f648a0eb592500a601d685a2a08cd0
minakdr <mina125.ak@gmail.com>
2025-01-01 21:37:16 +0100

    f

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,14 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './App.css';
+import './TodoList.css'
 
 
+const Segment = () => {
+    return ( 
+<div className='Segment'>
+
+
+<div className='Task'>
+<input type="checkbox" />
+<p>Clean kitchen </p>
+</div>
+
+<div className='Task'>
+<input type="checkbox" />
+<p>Cook dinner </p>
+</div>
+
+<div className='Task'>
+<input type="checkbox" />
+<p>Learn React </p>
+</div>
+
+</div>
+    )
+}
 
 
 
 function TodoList() {
 return ( 
-<h1>hey</h1>
+
+<div>
+<img className="img" src="./plants.jpg" alt="Plants" />
+<img className="img" src="./plants.jpg" alt="Plants" />
+<img className="img" src="./plants.jpg" alt="Plants" />
+<h1>My to-do List</h1>
+
+
+<Segment/>   
+<Segment/>   
+<Segment/>   
+<Segment/>   
+<Segment/>   
+
+</div>
+
 
 );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import TodoList from './TodoList';
+
+import TodoList from './TodoList.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
