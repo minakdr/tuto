b78886b2653f646d3e08b39cc880594a0bfc7e0e
minakdr <mina125.ak@gmail.com>
2025-01-01 18:33:22 +0100

    f

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './App.css';
 
+
+const MyInfos = () => {
+return (
+  <div>
+    <h1>Mina</h1>
+    <p>Blue is my favorite color , I love skyrim and I'm currently learning react form Youtube</p>
+    <ul style={{ listStyleType: "none" }}> 
+            <li>Germany</li>
+            <li>Italy</li>
+            <li>Budapest</li>
+          </ul>
+  </div>
+)
+
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return ( 
+    <div> {/* had to use  div because h1 and p are sibling elements and the retun needs to only return one parent element so I had to wrap them into 1 div  */}
+          <h1>Hello World</h1> 
+          <p>this a paragrph</p> 
+          <ul style={{ listStyleType: "none" }}> {/* Inline CSS also needs to be in camelCase */}
+            <li>first list item</li>
+            <li>second list item</li>
+            <li>third list item</li>
+          </ul>
+          <MyInfos/>
     </div>
+
   );
 }
 
