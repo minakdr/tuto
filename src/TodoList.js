a9aceb991d3c11ebf5d594550568f955d9841990
minakdr <mina125.ak@gmail.com>
2025-01-02 14:27:52 +0100

    o

--- src/TodoList.js
+++ src/TodoList.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './TodoList.css'
 
 
-const Segment = () => {
+const Segment = () => {  /* functional component so arrow fnct  */
     return ( 
 <div className='Segment'>
 
@@ -30,13 +30,33 @@ const Segment = () => {
 
 
 function TodoList() {
+
+const date = new Date()
+const hour = date.getHours()
+let timeOfday 
+let style = {}
+
+if (hour < 12) { 
+    timeOfday = 'morning?' ;
+    style.color ='rgb(99, 203, 255)';
+}
+else if ( hour < 18 ) { 
+    timeOfday = 'afternoon' ;
+    style.color ='rgb(130, 77, 4)';}
+else { 
+    timeOfday = 'evening'
+    style.color ='#1f2d20';
+}
+
+
 return ( 
 
 <div>
 <img className="img" src="./plants.jpg" alt="Plants" />
 <img className="img" src="./plants.jpg" alt="Plants" />
 <img className="img" src="./plants.jpg" alt="Plants" />
-<h1>My to-do List</h1>
+
+<h1 style={style}>Good {timeOfday}, feeling productive ?</h1>
 
 
 <Segment/>   
